e687d3dbefb575cf722feaba9bf2e11bf180bc9c
chenjh <1301793698@qq.com>
2018-11-03 03:02:37 +0800

    爬取所有主料和辅料

--- recipe/spiders/recipe_spider.py
+++ recipe/spiders/recipe_spider.py
@@ -20,12 +20,35 @@ class RSpider(scrapy.Spider):
             if response.xpath('//table[@class="retamr"]//tr[1]//td[@class="lirre"]/text()').extract_first() is not None:
                 item["difficulty"] = response.xpath('//table[@class="retamr"]//tr[1]//td[@class="lirre"]/text()').extract_first().strip()
             #时间
-            if response.xpath('//table[@class="retamr"]//span[contains(text(), "时间：")]').extract_first() is not None:
-                # item["time"] = response.xpath('//table[@class="retamr"]//span[contains(text(), "时间：")]').extract_first()             
+            if response.xpath('//table[@class="retamr"]//span[contains(text(), "时间：")]').extract_first() is not None:        
                 item["time"] = response.xpath('//table[@class="retamr"]//span[contains(text(), "时间：")]//../text()').extract()[2].strip().lstrip().rstrip(',')
-            # #主料
-            data = response.xpath('//table[@class="retamr"]//a[@target="_blank"]//..//..')
-            item["main_ingredient"] = data.xpath('string(.)').extract()[0].strip().lstrip().rstrip(',')
-            # #辅料
-            # minor_ingredient = scrapy.Field()
+            #主料
+            datas = response.xpath('//table[@class="retamr"]//h3[contains(text(), "主料")]//..//..//following-sibling::tr[1]//td')
+            content = []
+            order = 1
+            for data in datas:
+                # data_e = data.xpath('string(.)').extract()
+                ingredient = data.xpath(".//a/text()").extract_first()
+                numbers = data.xpath('.//span[@class="right"]/text()').extract_first()
+                content.append({
+                    '主料'+str(order): ingredient,
+                    '数量': numbers
+                })
+                order += 1
+            item["main_ingredient"] = content           
+            #辅料
+            datass = response.xpath('//table[@class="retamr"]//h3[contains(text(), "辅料")]//..//..//following-sibling::tr')
+            for datas in datass:
+                datas = datass.xpath('.//td')
+                content = []
+                order = 1
+                for data in datas:
+                    ingredient = data.xpath(".//label/text() | .//a/text()").extract_first()
+                    numbers = data.xpath('.//span[@class="right"]/text()').extract_first()
+                    content.append({
+                        '辅料'+str(order): ingredient,
+                        '数量': numbers
+                    })
+                    order += 1
+            item["minor_ingredient"] = content  
             return item
