07d03aff3a19d9bce72128bc30e00bdc586b5da1
chenjh <1301793698@qq.com>
2018-11-03 01:33:02 +0800

    爬取第一个主料&解决time标签爬取的问题

--- recipe/spiders/recipe_spider.py
+++ recipe/spiders/recipe_spider.py
@@ -5,6 +5,7 @@ from recipe.items import RecipeItem
 class RSpider(scrapy.Spider):
     name = "toscrape"
     start_urls = [
+        "https://www.douguo.com/cookbook/755193.html",
         "https://www.douguo.com/cookbook/1048927.html"
     ]
 
@@ -13,14 +14,18 @@ class RSpider(scrapy.Spider):
             #食谱名字
             item["name"] = response.xpath('//div[@class="recinfo"]//h1/text()').extract_first().strip()
             #食谱主要描述
-            item["description"] = response.xpath('//div[@class="retew r3 pb25 mb20"]//div[@class="xtip hidden"]/text()').extract_first().strip()
+            if response.xpath('//div[@class="retew r3 pb25 mb20"]//div[@class="xtip hidden"]/text()').extract_first() is not None:
+                item["description"] = response.xpath('//div[@class="retew r3 pb25 mb20"]//div[@class="xtip hidden"]/text()').extract_first().strip()
             #难度
-            item["difficulty"] = response.xpath('//table[@class="retamr"]//tr[1]//td[@class="lirre"]/text()').extract_first().strip()
+            if response.xpath('//table[@class="retamr"]//tr[1]//td[@class="lirre"]/text()').extract_first() is not None:
+                item["difficulty"] = response.xpath('//table[@class="retamr"]//tr[1]//td[@class="lirre"]/text()').extract_first().strip()
             #时间
-            item["time"] = response.xpath('//table[@class="retamr"]//tr[1]//td[2]/text()').extract()[1].strip().lstrip().rstrip(',')
+            if response.xpath('//table[@class="retamr"]//span[contains(text(), "时间：")]').extract_first() is not None:
+                # item["time"] = response.xpath('//table[@class="retamr"]//span[contains(text(), "时间：")]').extract_first()             
+                item["time"] = response.xpath('//table[@class="retamr"]//span[contains(text(), "时间：")]//../text()').extract()[2].strip().lstrip().rstrip(',')
             # #主料
-            # item["time"] = response.xpath('//table[@class="retamr"]//tr[2]//h3[@class="zfliao"]/text()').extract_first().strip()
-            # main_ingredient = scrapy.Field()
+            data = response.xpath('//table[@class="retamr"]//a[@target="_blank"]//..//..')
+            item["main_ingredient"] = data.xpath('string(.)').extract()[0].strip().lstrip().rstrip(',')
             # #辅料
             # minor_ingredient = scrapy.Field()
             return item
